8d44393cdaca0ea165f525fab6ea4adf5617b488
xZeyom <andreluisleiteguimaraes027@gmail.com>
2025-08-03 13:54:13 -0300

    teste3

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(__dirname));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/pedido", async (req, res) => {
