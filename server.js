601de775fb1c59c62ca7de9f739d49e595550ed0
xZeyom <andreluisleiteguimaraes027@gmail.com>
2025-08-03 17:07:12 -0300

    cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const { Pool } = require("pg");
 const path = require("path");
-
 const app = express();
+const cors = require('cors');
+
 app.use(cors());
+
 app.use(express.json());
 
 app.use(express.static(__dirname));
